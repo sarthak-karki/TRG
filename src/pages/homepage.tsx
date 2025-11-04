fae2f55fbdc9200e6973b6eb1324e5e9cd7628dc
Sarthak <sarthak.npl@gmail.com>
2025-11-04 21:56:29 +1100

    Add MeetTheTeam component and player images to the project

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,17 +1,20 @@
 import Header from "@app/components/header";
 import Footer from "@app/components/footer";
-
+import MeetTheTeam from "@app/components/MeetTheTeam";
 import TeamPhoto from "@app/assets/team.jpg";
 
 const HomePage = () => {
   return (
     <div className="bg-white leading-tight font-sans w-full">
       <Header />
+
       <main className="block box-border w-full">
         <div className="flex items-center w-full">
-          <img src={TeamPhoto} className="w-full"></img>
+          <img src={TeamPhoto} className="w-full max-h-1/2 object-cover" />
         </div>
       </main>
+
+      <MeetTheTeam />
       <Footer />
     </div>
   );
